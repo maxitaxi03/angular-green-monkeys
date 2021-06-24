af8d5ac754215672f3e7bb9ab700c3595358b1e2
Maximus Boxill <maximusboxill74@gmail.com>
2021-06-24 17:40:17 -0400

    Trying to implement toggle on monkey card

--- src/app/components/troop/troop.component.ts
+++ src/app/components/troop/troop.component.ts
@@ -12,6 +12,7 @@ export class TroopComponent implements OnInit {
   // troop?: Troop;
   @Input('troop') troop?: Troop;
   @Output() monkeySelected = new EventEmitter<Monkey>();
+  toggleCard = false;
   
   selectedMonkey?: Monkey;
   logger = '';
