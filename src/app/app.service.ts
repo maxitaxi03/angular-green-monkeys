2d411973b58a2ba562fc5dda888a25df70d40193
Maximus <maximusboxill74@gmail.com>
2021-07-04 21:27:37 -0400

    minor changes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,15 +6,15 @@ import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
   private _region!: Region;
   private _activeTroop$!: Observable<Troop>;
 
-  constructor() { }
+  constructor() {}
   get region(): Region {
-     return this._region;
+    return this._region;
   }
   get activeTroop(): Observable<Troop> {
     return this._activeTroop$;
@@ -27,7 +27,9 @@ export class AppService {
       // if not search term, return empty troop array.
       return of([]);
     }
-    return of(this._region.troops.filter(troop => troop.name.startsWith(term)));
+    return of(
+      this._region.troops.filter((troop) => troop.name.startsWith(term))
+    );
   }
   searchMonkeys(term: string): Observable<Monkey[]> {
     if (!term.trim()) {
@@ -37,7 +39,7 @@ export class AppService {
     return of([]);
   }
   createRegion(name: string): void {
-    this._region = new Region(name);  
+    this._region = new Region(name);
   }
   addNewTroopToRegion(name: string): void {
     this._region.createNewTroop(name);
@@ -45,14 +47,13 @@ export class AppService {
   findTroop(name: string): Troop | undefined {
     return this._region.findTroop(name);
   }
-  fetchOrders = async(userId: any) => {
-    return `${userId}'s order data`
-  }
+  fetchOrders = async (userId: any) => {
+    return `${userId}'s order data`;
+  };
   user$ = of({ uid: Math.random() });
   orders$ = this.user$.pipe(
-    map(user => {
-        return this.fetchOrders(user.uid);
+    map((user) => {
+      return this.fetchOrders(user.uid);
     })
   );
-
 }
