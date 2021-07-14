84272db3fd34da39ceaceacd6974c16b859816de
Maximus Boxill <maximus@pellucidit.com>
2021-07-14 11:38:30 -0400

    added monkey form

---
+++ src/app/models/monkey-form.model.ts
@@ -0,0 +1,13 @@
+export class Form {
+    name: String;
+    age: String;
+    weight: String;
+    gender: String;
+
+    constructor() {
+        this.name = '';
+        this.age = '';
+        this.weight = '';
+        this.gender = '';
+    }
+}
