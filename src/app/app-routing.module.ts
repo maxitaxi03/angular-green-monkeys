371b9c8db6fc2322667a8a3816056f5ba91a5777
Maximus Boxill <maximus@pellucidit.com>
2021-07-22 13:50:03 -0400

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TroopComponent } from './components/troop/troop.component';
+import { MonkeyComponent } from './components/monkey/monkey.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'troops', component: TroopComponent},
+  // may need to do a nested route - id will be the child of both troops and monkeys
+  {path: 'monkeys', component: MonkeyComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
