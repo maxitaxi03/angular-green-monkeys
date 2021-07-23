531285e288db42136e9c0b6f360cb31e4ab7bf73
Maximus Boxill <maximus@pellucidit.com>
2021-07-23 16:54:37 -0400

    troop component doesn't show unless <a> is clicked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TroopComponent } from './components/troop/troop.component';
 import { MonkeyComponent } from './components/monkey/monkey.component';
+import { MonkeyFormComponent } from './components/monkey-form/monkey-form.component';
 
 const routes: Routes = [
-  {path: 'troops', component: TroopComponent},
+  {
+    path: 'troops',
+    component: TroopComponent,
+    children: [
+      {
+        path: 'id',
+        component: TroopComponent,
+
+        children: [
+          {
+            path: 'monkeys',
+            component: MonkeyComponent,
+            children: [{ path: 'id', component: MonkeyFormComponent }],
+          },
+        ],
+      },
+    ],
+  },
   // may need to do a nested route - id will be the child of both troops and monkeys
-  {path: 'monkeys', component: MonkeyComponent} 
+  { path: 'monkeys', component: MonkeyComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
